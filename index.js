557249bdd36453608caa64967d69e994e60688f5
John Gaitho <gaithoj@gmail.com>
2021-12-28 16:48:20 +0300

    update parser

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const rootDir = process.cwd();
 
-const includeFiles = require("include-files");
+const includeFiles = require('include-files');
 
 const globals = {};
 
@@ -39,10 +39,11 @@ includeFiles.exists(
 );
 
 module.exports = {
-  parser: "babel-eslint",
-  extends: "airbnb-base",
+  parser: '@babel/eslint-parser',
+  extends: 'airbnb-base',
   parserOptions: {
     ecmaVersion: 8,
+    requireConfigFile: false,
   },
   globals: {
     sails: true,
@@ -62,33 +63,33 @@ module.exports = {
     ...globals,
   },
   rules: {
-    "no-underscore-dangle": "off",
-    "no-await-in-loop": "off",
-    quotes: "off",
-    "no-use-before-define": ["error", { functions: false, classes: true }],
-    "no-global-assign": "error",
-    "no-restricted-syntax": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/no-unresolved": "off",
-    "no-console": "off",
-    "prefer-destructuring": "off",
-    "no-async-promise-executor": "off",
-    "no-param-reassign": "off",
-    "object-curly-newline": "off",
-    "implicit-arrow-linebreak": "off",
-    "spaced-comment": "off",
-    "wrap-iife": "off",
-    "operator-linebreak": "off",
-    "no-useless-catch": "off",
-    "no-shadow": "off",
-    "arrow-body-style": "off",
-    "object-shorthand": "off",
-    "max-len": "off",
-    "global-require": "off",
-    "no-extend-native": "off",
-    indent: "off",
-    "func-names": ["error", "as-needed"],
-    "guard-for-in": "off",
+    'no-underscore-dangle': 'off',
+    'no-await-in-loop': 'off',
+    quotes: 'off',
+    'no-use-before-define': ['error', { functions: false, classes: true }],
+    'no-global-assign': 'error',
+    'no-restricted-syntax': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'no-console': 'off',
+    'prefer-destructuring': 'off',
+    'no-async-promise-executor': 'off',
+    'no-param-reassign': 'off',
+    'object-curly-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'spaced-comment': 'off',
+    'wrap-iife': 'off',
+    'operator-linebreak': 'off',
+    'no-useless-catch': 'off',
+    'no-shadow': 'off',
+    'arrow-body-style': 'off',
+    'object-shorthand': 'off',
+    'max-len': 'off',
+    'global-require': 'off',
+    'no-extend-native': 'off',
+    indent: 'off',
+    'func-names': ['error', 'as-needed'],
+    'guard-for-in': 'off',
     radix: [0],
   },
 };
