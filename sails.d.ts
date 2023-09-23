a57edfd41c284c8dcfb0df430f6be55d4c023399
Bernard Gaitho <gaithoben@gmail.com>
2023-09-23 11:49:22 +0300

    migration to nextjs

--- sails.d.ts
+++ sails.d.ts
@@ -88,10 +88,10 @@ declare namespace Sails {
     SystemSettings: ISystemSettings;
     getSystemSettings(): ISystemSettings;
     updateSystemSettings(
-      params: (SystemSettings: ISystemSettings) =>
-        | {
+      params:
+        | ((SystemSettings: ISystemSettings) => {
             [key: string]: any;
-          }
+          })
         | {
             [key: string]: any;
           }
